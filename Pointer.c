194e7a1bfccd40f28da763c01773f4e6432b14b6
Saniya-Vaghani <saniyavaghani1105@gmail.com>
2023-04-24 12:54:21 +0530

    firstprogram

---
+++ Pointer.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+main()
+{
+	int a=25,b=30,c=46;
+	int *ptr;
+	int **ptr1;
+	int ***ptr2;
+	ptr=&a;
+	ptr1=&b;
+	ptr2=&c;
+	printf("%d",&a);
+	printf("\n%d",ptr);
+	printf("\n%d",ptr1);
+	printf("\n%d",ptr2);
+	
+
+	
+
+	
+}

be676106e6cce064c51c2aa13386e1330c562435
Saniya-Vaghani <131648803+Saniya-Vaghani@users.noreply.github.com>
2023-04-25 20:12:49 +0530

    Add files via upload

---
+++ L1_2.BAK
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+  clrscr();
+  printf("NAME : Saniya Vaghani\n");
+  printf("AGE : 17\n");
+  printf("SCHOOL : P.P. Savani Chaitanya Vidhya Sankul");
+  getch();
+
+}
---
+++ L1_2_2.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+ clrscr();
+ printf("-\t-\t-\t-\t-\t-\t-\t-\n");
+ printf("|\t\t\t\t\t\t\t|\n");
+ printf("R\t\t\t\t\t\t\t|\n");
+ printf("N\t\t\t\t\t\t\t|\n");
+ printf("W\t\t\t\t\t\t\t|\n");
+ printf("|\t\t\t\t\t\t\t|\n");
+ printf("-\t-\t-\t-\t-\t-\t-\t-\n");
+
+
+
+
+ getch();
+
+}
